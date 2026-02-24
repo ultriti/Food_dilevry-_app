57b015b30faeaad2743d02e9ec9bea17d849526f
yashash ravi <yashashravi1@gmail.com>
2026-02-24 12:35:44 +0530

    user -> get profil , update profile

--- backend/Controller/User.controller.js
+++ backend/Controller/User.controller.js
@@ -19,19 +19,19 @@ module.exports.RegisterUser = async (req, res, next) => {
         }
 
         const newUser = new userModel({
-            email : Email,
-            password : Password,
-            userName : Username,
-            role : "USER",
-            phoneNumber : req.body.phoneNumber || null
-            
+            email: Email,
+            password: Password,
+            userName: Username,
+            role: "USER",
+            phoneNumber: req.body.phoneNumber || null
+
 
         })
         await newUser.save()
 
         return res.status(201).json({ message: "user registered successfully" })
 
-        
+
 
 
 
@@ -93,4 +93,36 @@ module.exports.getUserProfile = async (req, res, next) => {
     }
 }
 
+// update user profile
+module.exports.updateUserProfile = async (req, res, next) => {
+    const userId = req.params.id;
+    const { Username, Email, Password, phoneNumber } = req.body;
 
+    try {
+        const updatedUser = await userModel.findById(userId);
+
+        if (!updatedUser) {
+            return res.status(404).json({ error: [{ message: "user not found" }] });
+        }
+
+        if (Username) {
+            if (updatedUser) { updatedUser.userName = Username };
+        }
+        if (Email) {
+            if (updatedUser) { updatedUser.email = Email };
+        }
+        if (Password) {
+            if (updatedUser) { updatedUser.password = Password };
+        }
+        if (phoneNumber) {
+            if (updatedUser) { updatedUser.phoneNumber = phoneNumber };
+        }
+
+        await updatedUser.save();
+        return res.status(200).json({ message: "user updated successfully", user: updatedUser });
+
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: [{ message: "internal server error" }] });
+    }
+}
--- backend/router/User.route.js
+++ backend/router/User.route.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const router = express.Router()
-const {RegisterUser, userLogin, getUserProfile} = require("../Controller/User.controller")
+const {RegisterUser, userLogin, getUserProfile, updateUserProfile} = require("../Controller/User.controller")
 
 router.post("/Register",RegisterUser)
 router.post("/Login",userLogin)
 
 router.get("/getUserProfile/:id",getUserProfile)
+router.put("/updateUserProfile/:id",updateUserProfile)
+
+
 
 
 
